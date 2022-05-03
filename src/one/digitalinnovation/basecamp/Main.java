86f6cf9554dd76004650042c4d734ae68b5cfdf2
Stefanie Cristinie <ste.cristinie@hotmail.com>
2022-05-03 00:06:49 -0300

    vto java 2

---
+++ src/one/digitalinnovation/basecamp/Main.java
@@ -0,0 +1,44 @@
+package one.digitalinnovation.basecamp;
+
+/**
+ * Classe de exemplo para o exercício da Aula 2 de Variáveis, Tipos de Dados e Operadores aritméticos.
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        byte b1 = 10;
+        byte b2 = 20;
+
+        short s1 = 20000;
+
+        int i1 = 28500;
+
+        long l1 = 1000000000000000000L;
+
+        float f1 = 10.68f;
+
+        double d1 = 85.69;
+
+        char c1 = 'W';
+
+        String st1 = "Fulano";
+
+        boolean bo1 = true;
+        boolean bo2 =  false;
+
+        System.out.println(b1);
+        System.out.println(b2);
+        System.out.println(s1);
+        System.out.println(i1);
+        System.out.println(l1);
+        System.out.println(f1);
+        System.out.println(d1);
+        System.out.println(c1);
+        System.out.println(st1);
+        System.out.println(bo1);
+        System.out.println(bo2);
+        
+    }
+}
